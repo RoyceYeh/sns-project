402937a57c2ce863efd4ac272b22606592dfb3e8
LIONMAIL\bryceyeh <bryceyeh@liontravel.com>
2023-08-23 17:46:29 +0800

    調整App、Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import "./App.css";
-import Header from "./Header";
-import { BrowserRouter } from "react-router-dom";
+import './App.css'
+import Header from './Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/signin'
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Header />
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/signin'} element={<Signin />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,23 +1,37 @@
-import React from "react";
-import { Menu, Search } from "semantic-ui-react";
-import { Link } from "react-router-dom";
+import React from 'react'
+// import { Menu, Search } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { Menu, MenuItem } from './components/Menu/Menu'
+import Search from './components/Search/Search'
 
 const Header = () => {
-	return (
-		<Menu>
-			<Menu.Item as={Link} to='/'>
-				Social
-			</Menu.Item>
-			<Menu.Item>
-				<Search />
-			</Menu.Item>
-			<Menu.Menu position='right'>
-				<Menu.Item as={Link} to='/signin'>
-					登入/註冊
-				</Menu.Item>
-			</Menu.Menu>
-		</Menu>
-	);
-};
+  return (
+    <Menu>
+      <Link className="relative no-underline leading-none flex-[0_0_auto] select-none bg-[0_0] px-[1.25rem] py-[1.5rem] text-zinc-400 text-xl border-r-2" to="/">
+        Curious
+      </Link>
+      <MenuItem>
+        <Search />
+      </MenuItem>
+      <Link className="border-l-2 block ml-auto relative no-underline leading-none flex-[0_0_auto] select-none bg-[0_0] px-[1.25rem] py-[1.5rem] text-zinc-400 text-base hover:text-slate-800 " to="/signin">
+        登入/註冊
+      </Link>
+    </Menu>
 
-export default Header;
+    // <Menu>
+    //   <Menu.Item as={Link} to="/">
+    //     Social
+    //   </Menu.Item>
+    //   <Menu.Item>
+    //     <Search />
+    //   </Menu.Item>
+    //   <Menu.Menu position="right">
+    //     <Menu.Item as={Link} to="/signin">
+    //       登入/註冊
+    //     </Menu.Item>
+    //   </Menu.Menu>
+    // </Menu>
+  )
+}
+
+export default Header
